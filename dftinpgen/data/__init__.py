155946c2c3d4c703af951c9b3a202fd90660128f
Vinay Hegde <vhegde@citrine.io>
2019-09-19 11:27:34 -0700

    Add the data module (for e.g. elemental constants, etc)

---
+++ dftinpgen/data/__init__.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+"""
+Standard atomic weights from
+https://www.nist.gov/pml/atomic-weights-and-isotopic-compositions-relative-atomic-masses.
+
+Filtered as follows:
+- uncertainties in the `Standard Atomic Weight` column is ignored
+- if the standard atomic weight is a range [a, b], the value used here is the
+  mean. Note that the range encompasses atomic weight values of all normal
+  materials, and does not imply any statistical distribution (i.e., the mean
+  is not necessarily the most likely value).
+
+"""
+saw_file = os.path.join(os.path.dirname(__file__),
+                        'standard_atomic_weights.json')
+with open(saw_file) as fr:
+    STANDARD_ATOMIC_WEIGHTS = json.load(fr)
