0daa873d5a693078a516b259efaf5cd46051efb9
Vinay Hegde <vhegde@citrine.io>
2019-09-19 17:46:41 -0700

    Read in QE tags and groups into a global variable

--- dftinpgen/qe/settings/__init__.py
+++ dftinpgen/qe/settings/__init__.py
@@ -1,1 +1,9 @@
+import os
+import json
+
 from dftinpgen.qe.settings.base_recipes import *
+
+
+tags_file = os.path.join(os.path.dirname(__file__), 'tags_and_groups.json')
+with open(tags_file, 'r') as fr:
+    QE_TAGS = json.load(fr)
