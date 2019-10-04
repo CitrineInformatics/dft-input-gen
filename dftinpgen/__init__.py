54be4ec0fc92434f404859ff52d9a3932e3a40c1
Vinay Hegde <vhegde@citrine.io>
2019-10-03 23:24:17 -0700

    Add single-source version info

--- dftinpgen/__init__.py
+++ dftinpgen/__init__.py
@@ -1,3 +1,12 @@
+import os
+
 from dftinpgen.data import *
 from dftinpgen.qe import *
 from dftinpgen.vasp import *
+
+
+# single-sourcing the package version
+with open(os.path.join(os.path.dirname(__file__), 'VERSION.txt')) as fr:
+    __version__ = fr.read().strip()
+VERSION = __version__
+__short_version__ = __version__.rpartition('.')[0]
