d5632b949c2973e96d8369cbba81ae80868a73a6
Vinay Hegde <vhegde@citrine.io>
2019-10-28 17:59:44 -0700

    formatting: empty line to stop flake from complaining

--- src/dftinpgen/__init__.py
+++ src/dftinpgen/__init__.py
@@ -7,5 +7,6 @@ from dftinpgen.qe import *
 # single-sourcing the package version
 with open(os.path.join(os.path.dirname(__file__), 'VERSION.txt')) as fr:
     __version__ = fr.read().strip()
+
 VERSION = __version__
 __short_version__ = __version__.rpartition('.')[0]
