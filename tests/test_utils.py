836909098bc8830478256e9267b90bb279268234
Vinay Hegde <vhegde@citrine.io>
2019-10-28 18:49:36 -0700

    unittest -> pytest for internal consistency

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,18 +1,13 @@
-import unittest
+"""Unit tests for helper utilities in :mod:`dftinpgen.utils`."""
+
+import pytest
 
 from dftinpgen.utils import get_elem_symbol
 from dftinpgen.utils import DftinpgenUtilsError
 
 
-class TestUtils(unittest.TestCase):
-    """Unit test for helper utilities in :mod:`dftinpgen.utils`."""
-
-    def test_get_elem_symbol(self):
-        self.assertEqual(get_elem_symbol('Fe-34'), 'Fe')
-        self.assertEqual(get_elem_symbol('3RGe-34'), 'Ge')
-        with self.assertRaises(DftinpgenUtilsError):
-            print(get_elem_symbol('G23'))
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_get_elem_symbol():
+    assert get_elem_symbol('Fe-34') == 'Fe'
+    assert get_elem_symbol('3RGe-34') == 'Ge'
+    with pytest.raises(DftinpgenUtilsError):
+        print(get_elem_symbol('G23'))
