6f49ea9d48b2557b31c8284c85e1fd98a38ccdd8
Vinay Hegde <vhegde@citrine.io>
2020-11-12 01:04:18 -0800

    change pwx unit tests according to 721504bcb77bfc34da299add1e748139262ff3d6

--- tests/qe/test_pwx.py
+++ tests/qe/test_pwx.py
@@ -2,274 +2,237 @@
 
 import os
 import pytest
+from ase import io as ase_io
 
 from dftinpgen.qe.pwx import PwxInputGenerator
 from dftinpgen.qe.pwx import PwxInputGeneratorError
 from dftinpgen.qe.pwx import qe_val_formatter
 
 
-@pytest.fixture
-def pseudo_dir():
-    return os.path.join(os.path.dirname(__file__), 'files')
-
-
-@pytest.fixture
-def eg_struct():
-    def _eg_struct(fname):
-        return os.path.join(os.path.dirname(__file__), 'files', fname)
-    return _eg_struct
-
-
-@pytest.fixture
-def eg_pwx_in():
-    def _eg_pwx_in(fname, pseudo_dir=None):
-        files_dir = os.path.join(os.path.dirname(__file__), 'files')
-        if pseudo_dir is None:
-            pseudo_dir = files_dir
-        _pwx_in = os.path.join(files_dir, fname)
-        with open(_pwx_in, 'r') as fr:
-            return fr.read().format(pseudo_dir=pseudo_dir)
-    return _eg_pwx_in
+# define module-level variables used for testing
+# TODO hegdevinayi@gmail.com: refactor to fixtures (`pytest-data`)
+test_data_dir = os.path.join(os.path.dirname(__file__), "files")
+pseudo_dir = os.path.join(os.path.dirname(__file__), "files")
+feo_struct = ase_io.read(os.path.join(test_data_dir, "feo_conv.vasp"))
+al_fcc_struct = ase_io.read(os.path.join(test_data_dir, "al_fcc_conv.vasp"))
+fe_pseudo = os.path.join(test_data_dir, "fe_pbe_v1.5.uspp.F.UPF")
+o_pseudo = os.path.join(test_data_dir, "o_pbe_v1.2.uspp.F.UPF")
+al_pseudo = os.path.join(test_data_dir, "al_pbe_v1.uspp.F.UPF")
+with open(os.path.join(test_data_dir, "TEST_feo_conv_scf.in"), "r") as fr:
+    feo_scf_in = fr.read().format(pseudo_dir=pseudo_dir)
+with open(os.path.join(test_data_dir, "TEST_al_fcc_conv_scf.in"), "r") as fr:
+    al_fcc_scf_in = fr.read().format(pseudo_dir=pseudo_dir)
 
 
 def test_qe_val_formatter():
-    assert qe_val_formatter(True) == '.true.'
-    assert qe_val_formatter('False') == '"False"'
-    assert qe_val_formatter(12345) == '12345'
-    assert qe_val_formatter(1E-10) == '1e-10'
+    assert qe_val_formatter(True) == ".true."
+    assert qe_val_formatter("False") == '"False"'
+    assert qe_val_formatter(12345) == "12345"
+    assert qe_val_formatter(1e-10) == "1e-10"
 
 
-def test_set_params_from_structure(eg_struct):
-    # no crystal structure specified
-    pig = PwxInputGenerator()
-    assert not pig.custom_sett_dict
-    # normal functionality
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
-    assert pig.custom_sett_dict == {'nat': 4, 'ntyp': 2}
+def test_no_crystal_structure_input_error():
+    with pytest.raises(TypeError):
+        PwxInputGenerator()
 
 
-def test_get_pseudo_dir(pseudo_dir):
-    # no pseudo_dir/repo_dir/set specified
-    pig = PwxInputGenerator()
-    assert pig.get_pseudo_dir() is None
-    pig = PwxInputGenerator(custom_sett_dict={'pseudo_repo_dir': './'})
-    assert pig.get_pseudo_dir() is None
-    pig = PwxInputGenerator(custom_sett_dict={'pseudo_set': 'default'})
-    assert pig.get_pseudo_dir() is None
-    # default psp directory from recipe
-    pig = PwxInputGenerator(base_recipe='scf')
-    assert pig.get_pseudo_dir() == os.path.expanduser('~/pseudos/qe/default')
-    # user-specified pseudo repo dir and pseudo set
-    pig = PwxInputGenerator(custom_sett_dict={'pseudo_repo_dir': '.',
-                                              'pseudo_set': 'default'})
-    assert pig.get_pseudo_dir() == os.path.join('.', 'default')
-    # user-specified pseudo dir
-    pig = PwxInputGenerator(custom_sett_dict={'pseudo_dir': pseudo_dir})
-    assert pig.get_pseudo_dir() == pseudo_dir
+def test_parameters_from_structure():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
+    assert pwig.parameters_from_structure == {"nat": 4, "ntyp": 2}
 
 
-def test_get_psp_name(pseudo_dir, eg_struct):
+def test_get_pseudo_name():
     # specified pseudo_dir not found
-    _pseudo_dir = os.path.expanduser('~/missing_dir/default')
-    with pytest.raises(PwxInputGeneratorError):
-        PwxInputGenerator.get_psp_name('Fe34', _pseudo_dir)
-    # psp not found for species error
-    with pytest.raises(PwxInputGeneratorError):
-        print(PwxInputGenerator.get_psp_name('Cu', pseudo_dir))
-    # above cases with ignore missing; no error
-    assert PwxInputGenerator.get_psp_name(
-        'Fe34', _pseudo_dir, ignore_missing=True) is None
-    assert PwxInputGenerator.get_psp_name(
-        'Cu1', pseudo_dir, ignore_missing=True) is None
+    _pseudo_dir = os.path.expanduser("~/missing_dir/default")
+    with pytest.raises((FileNotFoundError, OSError)):
+        PwxInputGenerator._get_pseudo_name("Fe34", _pseudo_dir)
+    # pseudo not found for species
+    pseudo_name = PwxInputGenerator._get_pseudo_name("Cu", pseudo_dir)
+    assert pseudo_name is None
     # normal function
-    assert PwxInputGenerator.get_psp_name('Fe-34', pseudo_dir) == \
-        'fe_pbe_v1.5.uspp.F.UPF'
+    pseudo_name = PwxInputGenerator._get_pseudo_name("Fe-34", pseudo_dir)
+    assert pseudo_name == "fe_pbe_v1.5.uspp.F.UPF"
 
 
-def test_set_pseudopotentials(pseudo_dir, eg_struct):
-    # no crystal structure specified: no error
-    pig = PwxInputGenerator()
-    pig.set_pseudopotentials()
-    assert 'pseudo_dir' not in pig.calculation_settings
-    # ignore misssing psp: no errors
-    # crystal structure specified, no pseudo dir specified
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'))
-    pig.set_pseudopotentials(ignore_missing=True)
-    assert 'pseudo_dir' not in pig.calculation_settings
-    # crystal structure specified, non existing pseudo dir
-    pig.custom_sett_dict.update({'pseudo_dir': 'missing_dir'})
-    pig.set_pseudopotentials(ignore_missing=True)
-    assert pig.custom_sett_dict['psp_names']['Al'] is None
-    assert pig.custom_sett_dict['pseudo_dir'] == 'missing_dir'
-    # crystal structure specified, species psp missing errors
+def test_get_pseudo_names():
+    # do not setup potentials: no errors
+    pwig = PwxInputGenerator(crystal_structure=al_fcc_struct)
+    pwig.set_potentials = False
+    pseudo_names = pwig.get_pseudo_names()
+    assert pseudo_names == {"Al": None}
+    # crystal structure specified, non existing pseudo dir: no error
+    pwig.custom_sett_dict.update({"pseudo_dir": "missing_dir"})
+    pseudo_names = pwig.get_pseudo_names()
+    assert pseudo_names == {"Al": None}
+    # crystal structure specified, non existing pseudo dir: error
+    pwig.set_potentials = True
     with pytest.raises(PwxInputGeneratorError):
-        pig.set_pseudopotentials()
-    pig.custom_sett_dict.update({'pseudo_dir': os.path.expanduser('~')})
-    with pytest.raises(PwxInputGeneratorError):
-        pig.set_pseudopotentials()
+        pwig.get_pseudo_names()
     # normal functionality
-    pig.custom_sett_dict.update({'pseudo_dir': pseudo_dir})
-    pig.set_pseudopotentials()
-    assert pig.custom_sett_dict['pseudo_dir'] == pseudo_dir
-    print(pig.custom_sett_dict)
-    assert pig.custom_sett_dict['psp_names']['Al'] == \
-        'al_pbe_v1.uspp.F.UPF'
+    pwig.custom_sett_dict.update({"pseudo_dir": pseudo_dir})
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    pseudo_names = pwig.get_pseudo_names()
+    assert pseudo_names == {"Al": os.path.basename(al_pseudo)}
 
 
-def test_bare_base_calculation_settings(eg_struct):
-    pig = PwxInputGenerator()
-    assert not pig.calculation_settings
-    pig = PwxInputGenerator(
-        crystal_structure=eg_struct('al_fcc_conv.vasp'))
-    assert pig.calculation_settings == {'nat': 4, 'ntyp': 1}
+def test_bare_base_calculation_settings():
+    pwig = PwxInputGenerator(crystal_structure=al_fcc_struct)
+    calc_sett = pwig.get_calculation_settings()
+    assert calc_sett["nat"] == 4
 
 
-def test_scf_base_calculation_settings(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'),
-                            base_recipe='scf')
-    cs = pig.calculation_settings
-    assert cs['calculation'] == 'scf'
-    assert cs['namelists'] == ['control', 'system', 'electrons']
-    assert cs['kpoints']['scheme'] == 'automatic'
-    assert cs['kpoints']['shift'] == [0, 0, 0]
-    # test that pseudopotentials are set lazily
-    assert 'pseudo_dir' not in cs
-    pig.set_pseudopotentials(ignore_missing=True)
-    assert pig.calculation_settings['pseudo_dir'] == \
-        os.path.expanduser(os.path.join('~', 'pseudos', 'qe', 'default'))
+def test_scf_base_calculation_settings():
+    pwig = PwxInputGenerator(crystal_structure=al_fcc_struct)
+    pwig.base_recipe = "scf"
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    cs = pwig.calculation_settings
+    assert cs["calculation"] == "scf"
+    assert cs["namelists"] == ["control", "system", "electrons"]
+    assert cs["kpoints"]["scheme"] == "automatic"
+    assert cs["kpoints"]["shift"] == [0, 0, 0]
+    assert cs["pseudo_dir"] == os.path.join("~", "pseudos", "qe", "default")
 
 
-def test_relax_base_calculation_settings(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'),
-                            base_recipe='relax')
-    cs = pig.calculation_settings
-    assert cs['calculation'] == 'relax'
-    assert cs['namelists'] == ['control', 'system', 'electrons', 'ions']
-    assert 'pseudo_dir' not in cs
-    pig.set_pseudopotentials(ignore_missing=True)
-    assert pig.calculation_settings['pseudo_dir'] == \
-        os.path.expanduser(os.path.join('~', 'pseudos', 'qe', 'default'))
+def test_relax_base_calculation_settings():
+    pwig = PwxInputGenerator(crystal_structure=al_fcc_struct)
+    pwig.base_recipe = "relax"
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    cs = pwig.calculation_settings
+    assert cs["calculation"] == "relax"
+    assert cs["namelists"] == ["control", "system", "electrons", "ions"]
 
 
-def test_control_namelist_to_str(pseudo_dir, eg_struct, eg_pwx_in):
-    # control namelist without psp, settings
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
-    # 1. ignore missing psp; no error
-    nl = pig.namelist_to_str('control', ignore_missing_psp=True)
-    assert nl == '&CONTROL\n/'
-    # 2. do not ignore missing psp; throw error
+def test_control_namelist_to_str():
+    # control namelist without pseudo, settings: error
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
     with pytest.raises(PwxInputGeneratorError):
-        pig.namelist_to_str('control')
-    # with no psp, with settings
-    pig.custom_sett_dict.update({'calculation': 'scf'})
-    # 1. ignore missing psp; no error
-    nl = pig.namelist_to_str('control', ignore_missing_psp=True)
+        pwig.namelist_to_str("control")
+    # set_potentials = False: no error
+    pwig.set_potentials = False
+    nl = pwig.namelist_to_str("control")
+    assert nl == "&CONTROL\n/"
+    # with no pseudo, with settings
+    pwig.custom_sett_dict.update({"calculation": "scf"})
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    nl = pwig.namelist_to_str("control")
     assert nl == '&CONTROL\n    calculation = "scf"\n/'
-    # 2. do not ignore missing psp; throw error
+    # set_potentials = True: throw error
+    pwig.set_potentials = True
     with pytest.raises(PwxInputGeneratorError):
-        pig.namelist_to_str('control')
+        pwig.namelist_to_str("control")
     # normal functionality
-    pig.custom_sett_dict.update({'pseudo_dir': pseudo_dir})
-    pig.base_recipe = 'scf'
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    control = '\n'.join(scf_in.splitlines()[:7])
-    assert pig.namelist_to_str('control') == control
+    pwig.custom_sett_dict.update({"pseudo_dir": pseudo_dir})
+    pwig.base_recipe = "scf"
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    control = "\n".join(feo_scf_in.splitlines()[:7])
+    assert pwig.namelist_to_str("control") == control
 
 
-def test_namelist_to_str(pseudo_dir, eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'),
-                            base_recipe='scf',
-                            custom_sett_dict={'pseudo_dir': pseudo_dir})
-    scf_in = eg_pwx_in('TEST_al_fcc_conv_scf.in')
-    control = '\n'.join(scf_in.splitlines()[:7])
-    assert pig.namelist_to_str('control') == control
-    electrons = '\n'.join(scf_in.splitlines()[17:20])
-    assert pig.namelist_to_str('electrons') == electrons
+def test_namelist_to_str():
+    pwig = PwxInputGenerator(
+        crystal_structure=al_fcc_struct,
+        base_recipe="scf",
+        custom_sett_dict={"pseudo_dir": pseudo_dir},
+    )
+    control = "\n".join(al_fcc_scf_in.splitlines()[:7])
+    assert pwig.namelist_to_str("control") == control
+    electrons = "\n".join(al_fcc_scf_in.splitlines()[17:20])
+    assert pwig.namelist_to_str("electrons") == electrons
 
 
-def test_all_namelists_as_str(pseudo_dir, eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'),
-                            base_recipe='scf',
-                            custom_sett_dict={'pseudo_dir': pseudo_dir})
-    scf_in = eg_pwx_in('TEST_al_fcc_conv_scf.in', pseudo_dir=pseudo_dir)
-    namelists = '\n'.join(scf_in.splitlines()[:20])
-    assert pig.all_namelists_as_str == namelists
+def test_all_namelists_as_str():
+    pwig = PwxInputGenerator(
+        crystal_structure=al_fcc_struct,
+        base_recipe="scf",
+        custom_sett_dict={"pseudo_dir": pseudo_dir},
+    )
+    namelists = "\n".join(al_fcc_scf_in.splitlines()[:20])
+    assert pwig.all_namelists_as_str == namelists
 
 
-def test_get_atomic_species_card(eg_struct):
-    # without pseudo dir, with ignore missing
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
-    ref_line = 'O      15.99940000  None'
-    ac = pig.get_atomic_species_card(ignore_missing_psp=True)
+def test_get_atomic_species_card():
+    # set_potentials = False, no pseudo_dir: no error
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
+    pwig.set_potentials = False
+    ref_line = "O      15.99940000  None"
+    ac = pwig.atomic_species_card
     assert ac.splitlines()[-1] == ref_line
-    # without pseudo dir, without ignore missing; error
+    # set_potentials = True, no pseudo_dir: error
+    pwig.set_potentials = True
     with pytest.raises(PwxInputGeneratorError):
-        pig.get_atomic_species_card()
+        pwig.atomic_species_card
+    # normal functionality
+    pwig.custom_sett_dict.update({"pseudo_dir": pseudo_dir})
+    pwig.calculation_settings = pwig.get_calculation_settings()
+    ref_line = "O      15.99940000  {}".format(os.path.basename(o_pseudo))
+    ac = pwig.atomic_species_card
+    assert ac.splitlines()[-1] == ref_line
 
 
-def test_atomic_species_card(pseudo_dir, eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'),
-                            base_recipe='scf',
-                            custom_sett_dict={'pseudo_dir': pseudo_dir})
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    card = '\n'.join(scf_in.splitlines()[20:23])
-    assert pig.atomic_species_card == card
+def test_atomic_species_card():
+    pwig = PwxInputGenerator(
+        crystal_structure=feo_struct,
+        base_recipe="scf",
+        custom_sett_dict={"pseudo_dir": pseudo_dir},
+    )
+    card = "\n".join(feo_scf_in.splitlines()[20:23])
+    assert pwig.atomic_species_card == card
 
 
-def test_atomic_positions_card(eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'),
-                            base_recipe='scf')
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    card = '\n'.join(scf_in.splitlines()[23:28])
-    assert pig.atomic_positions_card == card
+def test_atomic_positions_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct, base_recipe="scf")
+    card = "\n".join(feo_scf_in.splitlines()[23:28])
+    assert pwig.atomic_positions_card == card
 
 
-def test_kpoints_card(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'),
-                            base_recipe='scf')
-    assert pig.kpoints_card == 'K_POINTS {automatic}\n7 7 7 0 0 0'
-    pig = PwxInputGenerator(crystal_structure=eg_struct('al_fcc_conv.vasp'),
-                            base_recipe='scf')
-    assert pig.kpoints_card == 'K_POINTS {automatic}\n8 8 8 0 0 0'
+def test_kpoints_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct, base_recipe="scf")
+    assert pwig.kpoints_card == "K_POINTS {automatic}\n9 9 9 0 0 0"
+    pwig = PwxInputGenerator(
+        crystal_structure=al_fcc_struct,
+        custom_sett_dict={"kpoints": {"scheme": "gamma"}},
+    )
+    assert pwig.kpoints_card == "K_POINTS {gamma}"
 
 
-def test_cell_parameters_card(eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    card = '\n'.join(scf_in.splitlines()[30:])
-    assert pig.cell_parameters_card == card
+def test_cell_parameters_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
+    card = "\n".join(feo_scf_in.splitlines()[30:])
+    assert pwig.cell_parameters_card == card
 
 
-def test_occupations_card(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
+def test_occupations_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
     with pytest.raises(NotImplementedError):
-        print(pig.occupations_card)
+        print(pwig.occupations_card)
 
 
-def test_constraints_card(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
+def test_constraints_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
     with pytest.raises(NotImplementedError):
-        print(pig.constraints_card)
+        print(pwig.constraints_card)
 
 
-def test_atomic_forces_card(eg_struct):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'))
+def test_atomic_forces_card():
+    pwig = PwxInputGenerator(crystal_structure=feo_struct)
     with pytest.raises(NotImplementedError):
-        print(pig.atomic_forces_card)
+        print(pwig.atomic_forces_card)
 
 
-def test_all_cards_as_str(pseudo_dir, eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'),
-                            base_recipe='scf',
-                            custom_sett_dict={'pseudo_dir': pseudo_dir})
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    all_cards = '\n'.join(scf_in.splitlines()[20:])
-    assert pig.all_cards_as_str == all_cards
+def test_all_cards_as_str():
+    pwig = PwxInputGenerator(
+        crystal_structure=feo_struct,
+        base_recipe="scf",
+        custom_sett_dict={"pseudo_dir": pseudo_dir},
+    )
+    all_cards = "\n".join(feo_scf_in.splitlines()[20:])
+    assert pwig.all_cards_as_str == all_cards
 
 
-def test_pwx_input_as_str(pseudo_dir, eg_struct, eg_pwx_in):
-    pig = PwxInputGenerator(crystal_structure=eg_struct('feo_conv.vasp'),
-                            base_recipe='scf',
-                            custom_sett_dict={'pseudo_dir': pseudo_dir})
-    scf_in = eg_pwx_in('TEST_feo_conv_scf.in')
-    assert pig.pwx_input_as_str == scf_in.rstrip('\n')
+def test_pwx_input_as_str():
+    pwig = PwxInputGenerator(
+        crystal_structure=feo_struct,
+        base_recipe="scf",
+        custom_sett_dict={"pseudo_dir": pseudo_dir},
+    )
+    assert pwig.pwx_input_as_str == feo_scf_in.rstrip("\n")
