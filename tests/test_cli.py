58977ece5d034c3590297e14a6c0f0c98c40171e
Vinay Hegde <vhegde@citrine.io>
2020-11-24 02:11:13 -0800

    add e2e test for normal func call in cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
 import os
 import pytest
 
-
 from dftinpgen.cli import get_parser
 from dftinpgen.cli import driver
+
+
+files_dir = os.path.join(os.path.dirname(__file__), "files")
+test_struct = os.path.join(files_dir, "feo_conv.vasp")
 
 
 def test_get_parser(capsys):
@@ -28,8 +31,27 @@ def test_driver(capsys):
     assert "invalid choice" in capsys.readouterr().err
 
     # pw.x package: missing required arguments error
-    # this is only a barebones test to ensure that the arguments have been
-    # passed on to the right subparser
+    # only tests that the arguments have been passed on to the pw.x subparser
     with pytest.raises(SystemExit):
         driver(["pw.x"])
     assert "required" in capsys.readouterr().err
+
+    # pw.x package: minimal working example
+    import tempfile
+
+    _tmp_file = tempfile.NamedTemporaryFile(mode="w", delete=True)
+    filename = _tmp_file.name
+    write_location = os.path.dirname(filename)
+    driver(
+        [
+            "pw.x",
+            "-i",
+            test_struct,
+            "-pre",
+            "scf",
+            "-loc",
+            write_location,
+            "-o",
+            filename,
+        ]
+    )
