227fc72d2a6ba3f52893a8f0898afcf8ae826b0d
Vinay Hegde <vhegde@citrine.io>
2020-11-23 22:47:28 -0800

    add dftinpgen cli driver entry point

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools import find_packages
 
 
-with open(os.path.join(os.path.dirname(__file__),
-                       'src', 'dftinpgen', 'VERSION.txt')) as fr:
+with open(
+    os.path.join(os.path.dirname(__file__), "src", "dftinpgen", "VERSION.txt")
+) as fr:
     version = fr.read().strip()
 
 
 setup(
-    name='dftinpgen',
+    name="dftinpgen",
     version=version,
-    description='Unopinionated library to generate input files for DFT codes',
-    url='https://github.com/CitrineInformatics/dft-input-gen',
-    author='Vinay Hegde',
-    author_email='vhegde@citrine.io',
-    packages=find_packages(where='src', exclude=['docs']),
-    package_dir={'': 'src'},
-    install_requires=[
-        'six',
-        'numpy',
-        'ase <= 3.17'
+    description="Unopinionated library to generate input files for DFT codes",
+    url="https://github.com/CitrineInformatics/dft-input-gen",
+    author="Vinay Hegde",
+    author_email="vhegde@citrine.io",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=2.7, >=3.8",
+    install_requires=["six", "numpy", "ase <= 3.17"],
+    entry_points={"console_scripts": ["dftinpgen = dftinpgen.cli:driver",]},
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    include_package_data=True,
 )
