9f5593b222b3ee7d86e0c8de319b61195cc58718
Vinay Hegde <vhegde@citrine.io>
2020-11-23 19:20:39 -0800

    add a barebones cli; use pwx argparse as subparser

---
+++ src/dftinpgen/cli.py
@@ -0,0 +1,32 @@
+import argparse
+
+from dftinpgen.demo.pwx import build_pwx_parser
+from dftinpgen.demo.pwx import generate_pwx_input_files
+
+
+def get_parser():
+    # create the top-level parser
+    description = """Input file generation for DFT codes."""
+    parser = argparse.ArgumentParser(description=description)
+    subparsers = parser.add_subparsers()
+
+    # add pw.x subparser
+    pwx_help = "Generate input file for pw.x (from the QE suite)"
+    pwx_parser = subparsers.add_parser("pw.x", help=pwx_help)
+    build_pwx_parser(pwx_parser)
+    pwx_parser.set_defaults(func=generate_pwx_input_files)
+
+    # other subparsers, to be added similarly, go here
+    # e.g. ones for gpaw/vasp
+
+    return parser
+
+
+def driver():
+    parser = get_parser()
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    driver()
